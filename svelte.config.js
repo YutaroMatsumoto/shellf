c66630821d577bd91954f386f374e8e581a89d4c
yutaro_matsumoto <spurs03170629@gmail.com>
2023-07-30 01:49:40 +0900

    alias設定

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$page: 'src/components/page',
+			'$page/*': 'src/components/page/*',
+			$model: 'src/components/model',
+			'$model/*': 'src/components/model/*'
+		}
 	}
-};
+}
 
-export default config;
+export default config
