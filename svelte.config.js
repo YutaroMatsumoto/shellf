069950a025d75ed4e855309fe584b7d8bcb48f93
yutaro_matsumoto <spurs03170629@gmail.com>
2023-07-30 11:08:21 +0900

    プロジェクト側の設定ファイル修正

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
+			'styled-system': './styled-system/*',
 			$page: 'src/components/page',
 			'$page/*': 'src/components/page/*',
 			$model: 'src/components/model',
