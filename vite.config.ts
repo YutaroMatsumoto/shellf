0eea060d3526d9529fda01d3fae1bf0c7ae1d867
yutaro matsumoto <66941804+YutaroMatsumoto@users.noreply.github.com>
2023-07-30 11:10:23 +0900

    pandacssの導入 (#2)
    * @pandacss/devのインストール
    
    * styled-systemの自動生成
    
    * pandacssの設定ファイル作成
    
    * プロジェクト側の設定ファイル修正
    
    * app.css作成
    
    * pandacssを試運転

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			/**
+			 * 参考 https://ja.vitejs.dev/config/server-options.html#server-fs-allow
+			 * プロジェクトのルート外からの特定のファイルやディクレトリを許可するための設定？
+			 * このオプションを使用して特定の場所からのインポートを許可することが可能。
+			 */
+			allow: ['styled-system']
+		}
+	}
+})
