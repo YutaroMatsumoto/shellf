84786cddcac3e61703ec302b27f00a04e25342e4
yutaro_matsumoto <spurs03170629@gmail.com>
2023-08-05 23:03:23 +0900

    Headerコンポーネントの実装

---
+++ src/components/ui/Header/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Logo from '$ui/Logo/Logo.svelte'
+	import { css } from 'styled-system/css'
+
+	const styles = css({
+		gridRow: '1',
+		display: 'flex',
+		alignItems: 'center',
+		width: '100%',
+		padding: '0 20px',
+		borderBottomWidth: '1px',
+		borderColor: 'gray.200'
+	})
+</script>
+
+<header class={styles}>
+	<Logo />
+</header>
+
+<style>
+</style>
