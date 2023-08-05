e62180db6b4a337f72445e8f2c76f5b6b46a42d0
yutaro_matsumoto <spurs03170629@gmail.com>
2023-08-05 23:03:35 +0900

    Logoコンポーネントの実装

---
+++ src/components/ui/Logo/Logo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	const styles = css({
+		color: 'green.500',
+		fontSize: '2xl',
+		lineHeight: '100%',
+		fontWeight: 'bold'
+	})
+</script>
+
+<a class={styles} href="/" aria-label="サイトのトップページへ遷移する"> Shellf</a>
