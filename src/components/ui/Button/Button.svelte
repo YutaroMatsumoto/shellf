60b4331224a3582e83c45ad1e2a708dde00e027d
yutaro_matsumoto <spurs03170629@gmail.com>
2023-08-11 11:20:08 +0900

    Buttonコンポーネントの作成

---
+++ src/components/ui/Button/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { button } from './recipe'
+
+	export let type: 'primary' | 'danger' = 'primary'
+	export let size: 'sm' | 'lg' = 'lg'
+	export let onClick: () => void
+</script>
+
+<button class={button({ size, type })} on:click={onClick}><slot /></button>
