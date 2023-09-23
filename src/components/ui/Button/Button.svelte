18ab524684310ed32926f90c18d8d35773d4ec9a
yutaro_matsumoto <spurs03170629@gmail.com>
2023-09-23 23:16:12 +0900

    古いコンポーネント削除

--- src/components/ui/Button/Button.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { button } from './recipe'
-
-	export let type: 'primary' | 'danger' = 'primary'
-	export let size: 'sm' | 'lg' = 'lg'
-	export let onClick: () => void
-</script>
-
-<button class={button({ size, type })} on:click={onClick}><slot /></button>
