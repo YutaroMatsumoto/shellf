bb6c0eb745228cb2e7160028f3096e5b8ddc04b7
yutaro_matsumoto <spurs03170629@gmail.com>
2023-09-23 23:15:12 +0900

    基本的なレイアウト実装

---
+++ src/components/ui/_layout/Footer/Footer.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	const styles = css({
+		gridRow: 3,
+		width: '100%',
+		padding: '20px',
+		backgroundColor: 'gray.200'
+	})
+
+	// type FooterNavigationList = {}
+	// const FOOTER_NAVIGATION_LIST: FooterNavigationList = {}
+</script>
+
+<footer class={styles}>
+	<ul>
+		<li>
+			<a href="/">利用規約</a>
+		</li>
+		<li>
+			<a href="/">利用規約</a>
+		</li>
+	</ul>
+</footer>
---
+++ src/components/ui/_layout/Layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Header from '$ui/_layout/Header/Header.svelte'
+	import Footer from '$ui/_layout/Footer/Footer.svelte'
+	import { css } from 'styled-system/css'
+	import { createModal } from '$globalStates/modal'
+	import Modal from '$ui/_layout/Modal/Modal.svelte'
+
+	const wrapperStyles = css({
+		display: 'grid',
+		gridTemplateRows: '58px 1fr auto',
+		height: '100vh'
+	})
+
+	const mainStyles = css({
+		gridRow: 2,
+		padding: '20px'
+	})
+
+	const modal = createModal
+</script>
+
+<div class={wrapperStyles}>
+	<Header />
+	<main class={mainStyles}>
+		<slot />
+	</main>
+	<Footer />
+</div>
+
+<!-- ここにmodalを差し込む仕組みを作る -->
+{#if $modal !== 'hidden'}
+	<Modal />
+{/if}
