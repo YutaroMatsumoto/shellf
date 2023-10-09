5867d9525bce29931c0a779e2c0cdc3ae9facd10
yutaro_matsumoto <spurs03170629@gmail.com>
2023-10-09 20:49:03 +0900

    pathNameを微修正

--- src/models/route.ts
+++ src/models/route.ts
@@ -1,6 +1,9 @@
 export const pathName = {
 	home: '/',
-	login: '/login'
+	login: '/login',
+	my: '/my',
+	groupNew: '/group/new'
 } as const
 
 export type PathName = keyof typeof pathName
+export type PathValues = (typeof pathName)[PathName]
