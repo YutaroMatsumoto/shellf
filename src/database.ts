521c3646ebb375cf42f8f06eb7abee30c4b511a8
yutaro_matsumoto <spurs03170629@gmail.com>
2023-10-14 12:09:59 +0900

    databaseファイルの更新

--- src/database.ts
+++ src/database.ts
@@ -9,6 +9,74 @@ export type Json =
 export interface Database {
   public: {
     Tables: {
+      group: {
+        Row: {
+          created_at: string
+          created_by: string | null
+          description: string | null
+          id: string
+          img_url: string | null
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          created_by?: string | null
+          description?: string | null
+          id?: string
+          img_url?: string | null
+          name: string
+        }
+        Update: {
+          created_at?: string
+          created_by?: string | null
+          description?: string | null
+          id?: string
+          img_url?: string | null
+          name?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "group_created_by_fkey"
+            columns: ["created_by"]
+            referencedRelation: "user"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      group_member: {
+        Row: {
+          created_at: string
+          group_id: string
+          id: number
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          group_id: string
+          id?: number
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          group_id?: string
+          id?: number
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "group_member_group_id_fkey"
+            columns: ["group_id"]
+            referencedRelation: "group"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "group_member_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "user"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       user: {
         Row: {
           avatar_url: string | null
