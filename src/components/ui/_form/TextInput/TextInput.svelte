869f3d322bc35dd12d59ceb8fff7f34133ddacfd
yutaro_matsumoto <spurs03170629@gmail.com>
2023-10-15 23:09:50 +0900

    TextInputコンポーネントの実装

---
+++ src/components/ui/_form/TextInput/TextInput.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let name: string
+	export let label: string
+	export let fildId: string
+
+	const fieldWrapper = css({
+		display: 'inline-flex',
+		padding: '4px',
+		borderWidth: '1px',
+		borderColor: 'gray.300',
+		borderRadius: 'sm',
+		_focusWithin: {
+			borderColor: 'green.300'
+		}
+	})
+</script>
+
+<div>
+	<label for={fildId}>{label}</label>
+	<span class={fieldWrapper}>
+		<input {name} id={fildId} type="text" />
+	</span>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+	}
+
+	label {
+		font-size: 12px;
+		font-weight: bold;
+	}
+
+	input {
+		border: none;
+		outline: none;
+	}
+</style>
