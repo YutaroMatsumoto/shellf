e480ab75068521124111fe3b9dd92eac0efead9d
yutaro_matsumoto <spurs03170629@gmail.com>
2023-10-22 12:36:16 +0900

    コメント追記

--- src/globalStates/supabase.ts
+++ src/globalStates/supabase.ts
@@ -1,3 +1,8 @@
+/**
+ * なくせる方法があればなくす。
+ * もしかしたら、event.locals.supabaseを使えば必要なくなるかも
+ * もしくは、ログイン/ログアウト機能で使うことのみを許容する。
+ */
 import { getContext, setContext } from 'svelte'
 import { readable, type Readable } from 'svelte/store'
 import type { SupabaseClient } from '@supabase/supabase-js'
