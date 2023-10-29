d17fa327622ceb5e6ccde455ea15c7c340d87bfa
yutaro_matsumoto <spurs03170629@gmail.com>
2023-10-29 17:36:47 +0900

    ErrorMessageコンポーネントの作成

---
+++ src/components/ui/_form/ErrorMessage/ErrorMessage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	const error = css({
+		fontSize: 'sm',
+		fontWeight: '600',
+		color: 'red.500'
+	})
+</script>
+
+<span class={error} {...$$restProps}><slot /></span>
