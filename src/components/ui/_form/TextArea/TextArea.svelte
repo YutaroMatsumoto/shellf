dc7829cbc0b103080419f95c06ece24719a008b6
yutaro_matsumoto <spurs03170629@gmail.com>
2023-10-29 17:38:16 +0900

    TextAreaコンポーネントの修正

--- src/components/ui/_form/TextArea/TextArea.svelte
+++ src/components/ui/_form/TextArea/TextArea.svelte
@@ -1,19 +1,43 @@
-<script lang="ts">
-	import { field } from '$ui/_form//commonStyle'
+<script lang="ts" context="module">
+	import type { AnyZodObject } from 'zod'
+	type T = AnyZodObject
+</script>
 
-	export let name: string
+<script lang="ts" generics="T extends AnyZodObject">
+	import ErrorMessage from '$ui/_form/ErrorMessage/ErrorMessage.svelte'
+	import { fieldStyle } from '$ui/_form//commonStyle'
+
+	import type { z } from 'zod'
+	import type { ZodValidation, FormPathLeaves } from 'sveltekit-superforms'
+	import { formFieldProxy, type SuperForm } from 'sveltekit-superforms/client'
+
+	// 基本的なhtml属性はrestPropsで受け取るようにする
+	export let form: SuperForm<ZodValidation<T>, unknown>
+	export let field: FormPathLeaves<z.infer<T>>
 	export let label: string
-	export let fildId: string
 
 	/** 初期行数 */
 	export let rows: number = 5
+	const { value, errors } = formFieldProxy(form, field)
+
+	const errormessageId = `errormessage-${$$restProps.id}`
 </script>
 
 <div>
-	<label for={fildId}>{label}</label>
-	<span class={field}>
-		<textarea {name} id={fildId} {rows} />
+	<label for={$$restProps.id}>{label}</label>
+	<span class={fieldStyle({ isError: !!$errors })}>
+		<textarea
+			name={field}
+			{rows}
+			bind:value={$value}
+			aria-invalid={$errors ? 'true' : undefined}
+			aria-errormessage={errormessageId}
+			{...$$restProps}
+		/>
 	</span>
+	{#if $errors}
+		<ErrorMessage id={errormessageId}>{$errors}</ErrorMessage>
+	{/if}
 </div>
 
 <style>
