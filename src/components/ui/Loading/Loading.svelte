5fb25e0b915f0fe87fd82e2e9e2168acba99b8f3
yutaro_matsumoto <spurs03170629@gmail.com>
2023-11-03 23:48:49 +0900

    loading uiの実装

---
+++ src/components/ui/Loading/Loading.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let size: number = 20
+
+	const loading = css({
+		width: `${size}px`,
+		height: `${size}px`,
+		borderRadius: '50%',
+		border: 'solid 2px',
+		borderTopColor: 'white',
+		borderRightColor: 'white',
+		borderBottomColor: 'white',
+		borderLeftColor: 'gray.300',
+		animationName: 'spin',
+		animationDuration: '1s',
+		animationIterationCount: 'infinite'
+	})
+</script>
+
+<div class={loading} />
