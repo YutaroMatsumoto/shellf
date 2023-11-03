8045ed73e031948cc21a56aa1e75fd944036b955
yutaro_matsumoto <spurs03170629@gmail.com>
2023-11-03 22:13:44 +0900

    グループ作成フォームのvalidationを修正

--- src/repositories/group/schema.ts
+++ src/repositories/group/schema.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod'
 
+// TODO: setErrorMapを使ったメッセージの指定が理想
+const requiredMessage = 'この項目は必須です'
+
 export const groupNewSchema = z.object({
-	name: z.string().max(10, { message: '10文字以下で入力してください' }),
-	description: z.string()
+	name: z
+		.string()
+		.max(10, { message: '10文字以下で入力してください' })
+		.min(1, { message: requiredMessage }),
+	description: z.string().min(1, { message: requiredMessage })
 })
 
 export type GroupNewSchema = typeof groupNewSchema
