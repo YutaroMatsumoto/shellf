df93d4d71175aaedb0ec9d4e255ee2830ab2bfd9
yutaro_matsumoto <spurs03170629@gmail.com>
2023-11-03 23:49:11 +0900

    PrimaryButtonの実装

---
+++ src/components/ui/_button/PrimaryButton/PrimaryButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Loading from '$ui/Loading/Loading.svelte'
+	import { primaryButton } from './recipe'
+
+	export let loading: boolean = false
+	export let size: 'sm' | 'lg' = 'lg'
+	export let onClick: () => void
+</script>
+
+<button class={primaryButton({ size })} on:click={onClick} {...$$restProps}>
+	{#if loading}
+		<Loading />
+	{:else}
+		<slot />
+	{/if}
+</button>
---
+++ src/components/ui/_button/PrimaryButton/recipe.ts
@@ -0,0 +1,32 @@
+/**
+ * buttonのRecipe
+ */
+import { cva } from 'styled-system/css'
+
+export const primaryButton = cva({
+	base: {
+		display: 'inline-flex',
+		border: 'none',
+		borderRadius: 'sm',
+		color: 'white',
+		fontWeight: 'bold',
+		transition: 'background',
+		cursor: 'pointer',
+		bg: 'green.500',
+		_hover: { bg: 'green.600' },
+		_loading: {
+			bg: 'green.300',
+			cursor: 'none',
+			pointerEvents: 'none'
+		}
+	},
+	variants: {
+		size: {
+			sm: { padding: '4px 8px', fontSize: '12px', lineHeight: '20px' },
+			lg: { padding: '8px 16px', fontSize: '16px', lineHeight: '24px' }
+		}
+	},
+	defaultVariants: {
+		size: 'lg'
+	}
+})
