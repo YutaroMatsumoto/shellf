e7137dcf3262d7b9e3045eb5712bd476a6045092
yutaro_matsumoto <spurs03170629@gmail.com>
2023-11-05 15:23:40 +0900

    AnchorButtonの微修正

--- src/components/ui/_button/AnchorButton/AnchorButton.svelte
+++ src/components/ui/_button/AnchorButton/AnchorButton.svelte
@@ -1,11 +1,11 @@
 <!-- このコンポーネントは使用頻度少なめにする -->
 <script lang="ts">
-	import { button } from '$ui/_button/recipe'
-	import type { PathValues } from 'src/models/route'
+	import type { PathValues } from '$lib/route'
+	import { primaryButton } from '$ui/_button/PrimaryButton/recipe'
 
 	export let title: string
 	export let href: PathValues | '#'
 	export let size: 'sm' | 'lg' = 'sm'
 </script>
 
-<a class={button({ size, type: 'primary' })} {href}>{title}</a>
+<a class={primaryButton({ size })} {href}>{title}</a>
