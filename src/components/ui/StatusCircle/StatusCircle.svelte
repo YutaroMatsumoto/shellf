af972b023b26f40d860f6d1a89d6fb042fcff498
yutaro_matsumoto <spurs03170629@gmail.com>
2023-11-20 01:01:59 +0900

    スナックバーに表示するステータスアイコンコンポーネント

---
+++ src/components/ui/StatusCircle/StatusCircle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Status } from '$globalStates/snackbar'
+	import { statusCircle } from './recipe'
+
+	export let status: Status | undefined
+</script>
+
+{#if status}
+	<div class={statusCircle({ status })} />
+{/if}
---
+++ src/components/ui/StatusCircle/recipe.ts
@@ -0,0 +1,11 @@
+import { cva } from 'styled-system/css'
+
+export const statusCircle = cva({
+	base: { width: '20px', height: '20px', marginRight: '20px', borderRadius: '50%' },
+	variants: {
+		status: {
+			success: { bg: 'green.500' },
+			failure: { bg: 'red.500' }
+		}
+	}
+})
