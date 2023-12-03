85972dc7ee12d8aaa3a27cf9668179de77199619
yutaro_matsumoto <spurs03170629@gmail.com>
2023-12-03 11:46:04 +0900

    database.tsの更新

--- src/database.ts
+++ src/database.ts
@@ -13,7 +13,7 @@ export interface Database {
         Row: {
           created_at: string
           created_by: string | null
-          description: string | null
+          description: string
           id: string
           img_url: string | null
           name: string
@@ -21,7 +21,7 @@ export interface Database {
         Insert: {
           created_at?: string
           created_by?: string | null
-          description?: string | null
+          description?: string
           id?: string
           img_url?: string | null
           name: string
@@ -29,7 +29,7 @@ export interface Database {
         Update: {
           created_at?: string
           created_by?: string | null
-          description?: string | null
+          description?: string
           id?: string
           img_url?: string | null
           name?: string
@@ -38,6 +38,7 @@ export interface Database {
           {
             foreignKeyName: "group_created_by_fkey"
             columns: ["created_by"]
+            isOneToOne: false
             referencedRelation: "user"
             referencedColumns: ["id"]
           }
@@ -48,30 +49,35 @@ export interface Database {
           created_at: string
           group_id: string
           id: number
+          is_admin: boolean
           user_id: string
         }
         Insert: {
           created_at?: string
           group_id: string
           id?: number
+          is_admin?: boolean
           user_id: string
         }
         Update: {
           created_at?: string
           group_id?: string
           id?: number
+          is_admin?: boolean
           user_id?: string
         }
         Relationships: [
           {
             foreignKeyName: "group_member_group_id_fkey"
             columns: ["group_id"]
+            isOneToOne: false
             referencedRelation: "group"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "group_member_user_id_fkey"
             columns: ["user_id"]
+            isOneToOne: false
             referencedRelation: "user"
             referencedColumns: ["id"]
           }
@@ -97,6 +103,7 @@ export interface Database {
           {
             foreignKeyName: "users_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
           }
@@ -107,7 +114,20 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      get_belong_group: {
+        Args: {
+          user_id: string
+        }
+        Returns: {
+          id: string
+          name: string
+          description: string
+          img_url: string
+          created_by: string
+          created_at: string
+          is_admin: boolean
+        }[]
+      }
     }
     Enums: {
       [_ in never]: never
@@ -117,3 +137,83 @@ export interface Database {
     }
   }
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
