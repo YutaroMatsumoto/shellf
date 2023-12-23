b1d160a0be999980e1366f671e6bfc6531c1f514
yutaro_matsumoto <spurs03170629@gmail.com>
2023-12-23 11:25:51 +0900

    GroupCardコンポーネント実装

---
+++ src/components/model/group/GroupList/GroupCard/GroupCard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { card, cardLink, img, noImg } from '$model/group/GroupList/GroupCard/groupCard.style'
+
+	export let id: string
+	export let name: string
+	export let imgSrc: string = ''
+
+	$: groupPageUrl = `/group/${id}`
+</script>
+
+<li>
+	<article class={card}>
+		<a class={cardLink} href={groupPageUrl}>
+			{#if imgSrc}
+				<img class={img} src={imgSrc} alt="group img" />
+			{:else}
+				<div class={noImg} />
+			{/if}
+			<h2>{name}</h2>
+		</a>
+	</article>
+</li>
+
+<style>
+	h2 {
+		padding: 5px 8px; /** NOTE: やりたくないが、y軸を5pxにしないとと3行目が途切れて表示されてしまう */
+		height: 48px;
+		line-height: 20px;
+		font-weight: 500;
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 2;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		white-space: normal;
+	}
+</style>
---
+++ src/components/model/group/GroupList/GroupCard/groupCard.style.ts
@@ -0,0 +1,30 @@
+import { css } from 'styled-system/css'
+import type { SystemStyleObject } from 'styled-system/types'
+
+const groupImgCommonStyle: SystemStyleObject = {
+	height: '72px'
+}
+
+export const card = css({
+	width: '160px',
+	height: '120px',
+	borderRadius: 'sm',
+	borderWidth: '1px',
+	borderColor: 'gray.300',
+	cursor: 'pointer'
+})
+
+export const cardLink = css({
+	h: 'full',
+	display: 'flex',
+	flexDirection: 'column'
+})
+
+export const noImg = css({
+	...groupImgCommonStyle,
+	bg: 'gray.200'
+})
+
+export const img = css({
+	...groupImgCommonStyle
+})
