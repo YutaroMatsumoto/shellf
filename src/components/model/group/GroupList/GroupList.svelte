b447ca81c6517ab6878bed878181e5340e6be336
yutaro_matsumoto <spurs03170629@gmail.com>
2023-12-23 11:26:16 +0900

    GroupListコンポーネント実装

---
+++ src/components/model/group/GroupList/GroupList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import GroupCard from '$model/group/GroupList/GroupCard/GroupCard.svelte'
+	import type { Groups } from 'src/models/group'
+
+	export let groups: Groups
+</script>
+
+<ul>
+	{#each groups as group (group.id)}
+		<GroupCard id={group.id} name={group.name} imgSrc={group.img_url} />
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 20px;
+	}
+</style>
