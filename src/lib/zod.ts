07379b46190c6c21ce97bfa64f0b2a4c4ee118cc
yutaro_matsumoto <spurs03170629@gmail.com>
2024-01-08 12:58:57 +0900

    zodのエラーメッセージの日本語化を試みた

---
+++ src/lib/zod.ts
@@ -0,0 +1,12 @@
+// import i18next from 'i18next'
+// import { z } from 'zod'
+// import { zodI18nMap } from 'zod-i18n-map'
+// import translation from 'zod-i18n-map/locales/ja/zod.json'
+
+// i18next.init({
+// 	lng: 'ja',
+// 	resources: {
+// 		ja: { zod: translation }
+// 	}
+// })
+// z.setErrorMap(zodI18nMap)
