6823639a1dc8daf22939b13aa7f9de09042c273e
yutaro_matsumoto <spurs03170629@gmail.com>
2024-01-20 10:48:22 +0900

    GroupPageTabを実装

---
+++ src/components/model/group/GroupPageTab/GroupPageTab.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores'
+	import Tab from '$ui/Tab/Tab.svelte'
+	import { getTabs } from './tab'
+
+	$: tabs = getTabs($page.params.id)
+</script>
+
+<Tab {tabs} />
---
+++ src/components/model/group/GroupPageTab/tab.ts
@@ -0,0 +1,14 @@
+import type { TabItemType } from '$ui/Tab/tab'
+
+/** 動的ルーティングなので、変数ではなく関数でtabを返すようにする */
+export function getTabs(id: string): TabItemType[] {
+	return [
+		{ id: '/group/[id]', href: `/group/${id}`, title: 'イベント', ariaControles: 'group-events' },
+		{
+			id: '/group/[id]/member',
+			href: `/group/${id}/member`,
+			title: 'メンバー',
+			ariaControles: 'group-member'
+		}
+	]
+}
