16f201ac030ac6c0068cc88b0f3c83c5f00a881f
yutaro_matsumoto <spurs03170629@gmail.com>
2024-01-20 10:46:45 +0900

    TabItemTypeの型を修正

--- src/components/ui/Tab/tab.ts
+++ src/components/ui/Tab/tab.ts
@@ -1,1 +1,2 @@
-export type TabItemType = { href: string; title: string; ariaControles: string }
+/** id等の動的ルーティングが必要な場合、idを活用する */
+export type TabItemType = { id?: string; href: string; title: string; ariaControles: string }
