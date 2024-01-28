6ec6badad112afbee8092ddf40752d8b14c84910
yutaro_matsumoto <spurs03170629@gmail.com>
2024-01-28 09:13:27 +0900

    AdministratorWithAvatarの表示場所を変更

--- src/components/model/group/GroupDetailCard/GroupDetailCard.svelte
+++ src/components/model/group/GroupDetailCard/GroupDetailCard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { GroupWithCreateUser } from 'src/models/group'
 	import { cardWrapper, groupName, groupImage } from './gorupDetailCard.style'
-	import AdministratorWithAvatar from './AdministratorWithAvatar/AdministratorWithAvatar.svelte'
 
 	export let group: GroupWithCreateUser
 </script>
@@ -9,5 +8,4 @@
 <article class={cardWrapper}>
 	<img class={groupImage} src={group.img_url} alt="group img" />
 	<h2 class={groupName}>{group.name}</h2>
-	<AdministratorWithAvatar name={group.display_name} src={group.avatar_url} />
 </article>
