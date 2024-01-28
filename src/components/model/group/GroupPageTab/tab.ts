ef2497860d4e734926b78c08572ccaeef57c3934
yutaro_matsumoto <spurs03170629@gmail.com>
2024-01-28 23:39:14 +0900

    group詳細画面のタブを追加

--- src/components/model/group/GroupPageTab/tab.ts
+++ src/components/model/group/GroupPageTab/tab.ts
@@ -3,7 +3,13 @@ import type { TabItemType } from '$ui/Tab/tab'
 /** 動的ルーティングなので、変数ではなく関数でtabを返すようにする */
 export function getTabs(id: string): TabItemType[] {
 	return [
-		{ id: '/group/[id]', href: `/group/${id}`, title: 'イベント', ariaControles: 'group-events' },
+		{ id: '/group/[id]', href: `/group/${id}`, title: 'ホーム', ariaControles: 'group-home' },
+		{
+			id: '/group/[id]/event',
+			href: `/group/${id}/event`,
+			title: 'イベント',
+			ariaControles: 'group-events'
+		},
 		{
 			id: '/group/[id]/member',
 			href: `/group/${id}/member`,
