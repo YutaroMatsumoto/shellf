17982737ebc9bd20ce8f39c939b33582055961d2
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-10 18:57:05 +0900

    Dividerコンポーネント実装

---
+++ src/components/ui/Divider/Divider.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { css } from 'styled-system/css'
+
+	const divider = css({
+		width: '1px',
+		margin: '0 8px',
+		backgroundColor: 'gray.300'
+	})
+</script>
+
+<div class={divider} />
