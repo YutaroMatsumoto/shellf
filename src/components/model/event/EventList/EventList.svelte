8aca06d08b8608ccabff475831e2a21dfd5a9288
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-20 02:52:00 +0900

    EventList

---
+++ src/components/model/event/EventList/EventList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import EventItem from '$model/event/EventList/EventeItem/EventItem.svelte'
+	import type { Event } from '$models/event'
+	export let events: Event[]
+</script>
+
+<ul>
+	{#each events as { id, title, start_datetime: startDatetime, has_time: hasTime } (id)}
+		<EventItem {id} {title} {startDatetime} {hasTime} />
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+	}
+</style>
