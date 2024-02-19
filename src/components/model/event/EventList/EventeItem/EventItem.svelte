83c1507de8615eb80a7308e0a89c2589cada359f
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-20 02:51:49 +0900

    EventItem作成

---
+++ src/components/model/event/EventList/EventeItem/EventItem.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import { generatePath } from '$lib/route'
+	import dayjs from '$lib/dayjs'
+	import { item } from './eventItem.style'
+	import EventLabel from '$model/event/EventLabel/EventLabel.svelte'
+
+	export let id: string
+	export let title: string
+	export let startDatetime: string
+	export let hasTime: boolean
+
+	$: eventPageUrl = generatePath('eventDetail', [$page.params.id, id])
+
+	const start = new Date(startDatetime)
+
+	$: isToday = dayjs(startDatetime).isToday()
+	$: isAfter = dayjs(startDatetime).isAfter()
+
+	$: formatdate = hasTime
+		? dayjs(startDatetime).format('YYYY/MM/DD（ddd）HH:mm~')
+		: dayjs(startDatetime).format('YYYY/MM/DD（ddd）')
+</script>
+
+<li>
+	<a href={eventPageUrl}>
+		<article class={item}>
+			<span>
+				{formatdate}
+			</span>
+			{#if isToday || isAfter}
+				<EventLabel status={isToday ? 'today' : 'future'} />
+			{/if}
+			<h2>{title}</h2>
+		</article>
+	</a>
+</li>
+
+<style>
+	h2 {
+		font-size: 16px;
+		font-weight: bold;
+	}
+</style>
---
+++ src/components/model/event/EventList/EventeItem/eventItem.style.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-system/css'
+
+export const item = css({
+	display: 'flex',
+	flexDirection: 'column',
+	gap: '8px',
+	padding: '16px',
+	borderBottomWidth: '1px',
+	borderColor: 'gray.300',
+	_hover: {
+		backgroundColor: 'gray.100'
+	}
+})
