9b79616ce11d91df3c24531c230e98aae56faee4
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-20 02:53:06 +0900

    evnet一覧画面実装

---
+++ src/routes/group/[id]/event/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+	import EventList from '$model/event/EventList/EventList.svelte'
+	import Spacer from '$ui/Spacer/Spacer.svelte'
+	import AnchorButton from '$ui/_button/AnchorButton/AnchorButton.svelte'
+	import type { PageData } from './$types'
+	import Pagination from '$ui/Pagination/Pagination.svelte'
+
+	export let data: PageData
+
+	const wrapper = css({
+		display: 'flex'
+	})
+
+	$: ({ group, events } = data)
+	$: groupData = group.data?.[0]
+
+	$: total = data.events.count ?? 0
+	$: currentPage = Number(data.currentPage) ?? 1
+</script>
+
+<div class={wrapper}>
+	<AnchorButton pathKey="eventNew" params={groupData?.id} title="イベント作成" />
+</div>
+<Spacer />
+{#if !events.data || events.count === 0}
+	所属グループはありません。
+{:else}
+	<EventList events={events.data} />
+{/if}
+{#if events.data}
+	<Pagination pathKey="groupEventList" params={groupData?.id} {total} {currentPage} />
+{/if}
