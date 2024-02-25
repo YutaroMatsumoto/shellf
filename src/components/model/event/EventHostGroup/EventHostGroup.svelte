7ae8a569bbead70fce4843e22308d0fc24e5a98a
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-25 23:28:59 +0900

    イベント詳細画面でイベントの管理者を表示

---
+++ src/components/model/event/EventHostGroup/EventHostGroup.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { GroupWithCreateUser } from '$models/group'
+	import { administratorSection, sectionWrapper, title, groupImage } from './eventHostGroup.style'
+
+	export let name: GroupWithCreateUser['name']
+	export let src: GroupWithCreateUser['img_url']
+</script>
+
+<section class={sectionWrapper}>
+	<h3 class={title}>開催グループ</h3>
+	<div class={administratorSection}>
+		<img class={groupImage} {src} alt="group img" />
+		<span>{name}</span>
+	</div>
+</section>
+
+<style>
+	div {
+		display: flex;
+		align-items: center;
+	}
+</style>
