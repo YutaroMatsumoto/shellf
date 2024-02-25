11b47d5c94c4cad95c1a0d46a0ec1a5152826c8b
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-25 11:47:18 +0900

    グループホームページ uiをcomponents/pageに移動

---
+++ src/components/page/group/[id]/GroupHomePage.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import AdministratorWithAvatar from '$model/group/AdministratorWithAvatar/AdministratorWithAvatar.svelte'
+	import AnchorButton from '$ui/_button/AnchorButton/AnchorButton.svelte'
+	import type { GroupWithCreateUser } from '$models/group'
+
+	export let group: undefined | GroupWithCreateUser
+</script>
+
+<div>
+	<div>
+		<AnchorButton pathKey="eventNew" params={group?.id} title="イベント作成" />
+	</div>
+	{#if group}
+		<AdministratorWithAvatar name={group.display_name} src={group.avatar_url} />
+	{/if}
+</div>
+
+<style>
+	div {
+		display: flex;
+	}
+</style>
