0ea33a27d0bfcf116290039d1b3a37763207662c
yutaro_matsumoto <spurs03170629@gmail.com>
2024-02-25 11:53:43 +0900

    TextLinkコンポーネントを実装

---
+++ src/components/ui/TextLink/TextLink.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let text: string
+	export let href: string = '#'
+	export let isBack: boolean = false
+
+	const anchor = css({
+		color: 'green.500',
+		textDecoration: 'underline',
+		cursor: 'pointer',
+		_hover: { color: 'green.600' }
+	})
+
+	function goBack() {
+		window.history.back()
+	}
+</script>
+
+{#if isBack}
+	<button class={anchor} on:click={goBack}>{text}</button>
+{:else}
+	<a class={anchor} {href}>{text}</a>
+{/if}
