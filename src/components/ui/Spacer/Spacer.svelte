79f0a24276d76969b192232b20819fb5fb9035e2
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-09 09:50:03 +0900

    Spacerコンポーネント修正

--- src/components/ui/Spacer/Spacer.svelte
+++ src/components/ui/Spacer/Spacer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { spacer } from './recipe'
 
-	export let space: 20 = 20
+	export let space: 20 | 24 = 20
 </script>
 
 <div class={spacer({ space })} />
--- src/components/ui/Spacer/recipe.ts
+++ src/components/ui/Spacer/recipe.ts
@@ -3,7 +3,8 @@ import { cva } from 'styled-system/css'
 export const spacer = cva({
 	variants: {
 		space: {
-			20: { marginTop: '20px' }
+			20: { marginTop: '20px' },
+			24: { marginTop: '24px' }
 		}
 	}
 })
