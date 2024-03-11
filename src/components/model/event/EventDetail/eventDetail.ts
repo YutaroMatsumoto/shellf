e01bbcbcc5482d2763adaf66c7bc030ffb4c190a
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-11 22:17:43 +0900

    イベント詳細画面のstyle修正

--- src/components/model/event/EventDetail/eventDetail.ts
+++ src/components/model/event/EventDetail/eventDetail.ts
@@ -4,18 +4,20 @@ import type { SystemStyleObject } from 'styled-system/types'
 export const imgAreaCommonStyle: SystemStyleObject = {
 	minWidth: '384px',
 	minHeight: '216px',
-	maxWidth: '980px',
+	maxHeight: '540px',
+	maxWidth: '960px',
 	objectFit: 'contain'
 }
 
 export const sectionWrapper = css({
+	// 最小400pxの想定 768px - 48px（padding） - 320px
 	width: 'calc(100% - 320px)'
 })
 
 export const section = css({
 	width: '100%',
-	minWidth: 420,
-	padding: '16px',
+	minWidth: 400,
+	padding: '8px',
 	backgroundColor: 'white',
 	border: '1px solid',
 	borderRadius: 'sm',
