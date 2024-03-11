4ec222144910aa15291e5230319bc948731528f6
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-11 12:15:59 +0900

    イベント詳細画面微修正

--- src/components/model/event/EventDetail/EventDetail.svelte
+++ src/components/model/event/EventDetail/EventDetail.svelte
@@ -8,7 +8,6 @@
 		section
 	} from '$model/event/EventDetail/eventDetail'
 	import Divider from '$ui/Divider/Divider.svelte'
-	import { css } from 'styled-system/css'
 	import SubHeading from '$ui/_heading/SubHeading/SubHeading.svelte'
 	import Spacer from '$ui/Spacer/Spacer.svelte'
 	import EventDescription from '$model/event/EventDescription/EventDescription.svelte'
@@ -24,7 +23,7 @@
 		<Divider />
 		<div class={imgWrapper}>
 			{#if src}
-				<img class={eventImg} {src} alt="event img" />
+				<img class={eventImg} {src} alt="event" />
 			{:else}
 				<p class={noImg}>画像がありません</p>
 			{/if}
