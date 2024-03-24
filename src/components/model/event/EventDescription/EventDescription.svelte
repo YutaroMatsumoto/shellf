af8e55072d542d345720c84716baed89884748ea
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-24 22:17:40 +0900

    イベントの説明をマークダウンで表示

--- src/components/model/event/EventDescription/EventDescription.svelte
+++ src/components/model/event/EventDescription/EventDescription.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import type { GroupWithCreateUser } from '$models/group'
+
+	import Markdown from '$ui/Markdown/Markdown.svelte'
 	import { descriptionSection, sectionWrapper, title } from './eventDescription'
 
 	export let description: GroupWithCreateUser['description'] | null
@@ -8,6 +10,6 @@
 <section class={sectionWrapper}>
 	<h3 class={title}>イベントの説明</h3>
 	<div class={descriptionSection}>
-		<span>{description}</span>
+		<Markdown md={description ?? ''} />
 	</div>
 </section>
