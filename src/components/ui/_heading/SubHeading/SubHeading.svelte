107ca81c8557f2f45d1046bda0e2acd0478b39cc
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-24 23:01:32 +0900

    h2タグコンポーネント

---
+++ src/components/ui/_heading/SubHeading/SubHeading.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let title: string
+	/** fontsizeもpropsで受け取れた方が良いかも */
+</script>
+
+<h2>{title}</h2>
+
+<style>
+	h2 {
+		font-size: 16px;
+		font-weight: bold;
+	}
+</style>
