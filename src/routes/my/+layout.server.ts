b6fb65ff83dc2c80133e9a53586ec90f7e7cc834
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-30 10:16:59 +0900

    マイページのlayout.serverで自身が作成したグループを取得

--- src/routes/my/+layout.server.ts
+++ src/routes/my/+layout.server.ts
@@ -2,9 +2,16 @@ export const prerender = false
 import type { LayoutServerLoad } from './$types'
 import { error } from '@sveltejs/kit'
 
-export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
+export const load: LayoutServerLoad = async ({ locals: { getSession, supabase } }) => {
 	const session = await getSession()
 	if (!session) {
 		error(401, 'ログインが必要です。')
 	}
+
+	const groupsCreatedByMe = await supabase
+		.from('group')
+		.select('*', { count: 'exact' })
+		.eq('created_by', session.user.id)
+
+	return { groupsCreatedByMe }
 }
