4db5bbcd2e7ca3ceade55268e1c8a257ff9cca44
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-31 22:56:47 +0900

    グループは2つまで

--- src/routes/my/+page.svelte
+++ src/routes/my/+page.svelte
@@ -7,9 +7,13 @@
 	export let data: PageData
 
 	$: ({ data: groups } = data.groups)
+	$: createdByMeGroupNum = groups?.filter(({ is_admin }) => is_admin).length ?? 0
+
+	$: prohibitCreateGroup = createdByMeGroupNum > 2 ? '作成できるグループは２つまでです。' : ''
 </script>
 
-<AnchorButton pathKey="groupNew" title="グループ作成" />
+<AnchorButton pathKey="groupNew" title="グループ作成" disabledReason={prohibitCreateGroup} />
+
 <Spacer />
 {#if !groups || groups.length === 0}
 	所属グループはありません。
