0ab0b8581acaf3e72c32317239b03d092263ad5d
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-31 16:57:09 +0900

    不要なAnchorButtonを削除

--- src/routes/group/[id]/+page.svelte
+++ src/routes/group/[id]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import AdministratorWithAvatar from '$model/group/AdministratorWithAvatar/AdministratorWithAvatar.svelte'
-	import AnchorButton from '$ui/_button/AnchorButton/AnchorButton.svelte'
 	import type { PageData } from './$types'
 
 	export let data: PageData
@@ -9,9 +8,6 @@
 </script>
 
 <div>
-	<div>
-		<AnchorButton pathKey="eventNew" params={groupData?.id} title="イベント作成" />
-	</div>
 	{#if groupData}
 		<AdministratorWithAvatar name={groupData.display_name} src={groupData.avatar_url} />
 	{/if}
