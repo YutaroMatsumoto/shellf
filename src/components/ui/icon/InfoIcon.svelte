a0c77c079a440fc36107e9c59d41e8380fd2280f
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-31 17:41:27 +0900

    InfoIconを実装

---
+++ src/components/ui/icon/InfoIcon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let size: number = 16
+
+	const wrapper = css({
+		display: 'flex'
+	})
+</script>
+
+<div class={wrapper}>
+	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 14 14">
+		<path
+			fill="gray"
+			fill-rule="evenodd"
+			d="M7 14A7 7 0 1 0 7 0a7 7 0 0 0 0 14M5.5 9.375a.625.625 0 1 0 0 1.25h3a.625.625 0 1 0 0-1.25h-.875V6.5A.625.625 0 0 0 7 5.875H6a.625.625 0 1 0 0 1.25h.375v2.25zM8 4a1 1 0 1 1-2 0a1 1 0 0 1 2 0"
+			clip-rule="evenodd"
+		/>
+	</svg>
+</div>
