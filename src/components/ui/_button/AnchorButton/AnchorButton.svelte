7f15a64821f3524f081bb718543ffa5d617cac8a
yutaro_matsumoto <spurs03170629@gmail.com>
2024-03-31 17:42:38 +0900

    AnchorButtonでdisabledReasonを受け取れるよう修正

--- src/components/ui/_button/AnchorButton/AnchorButton.svelte
+++ src/components/ui/_button/AnchorButton/AnchorButton.svelte
@@ -1,18 +1,34 @@
 <!-- このコンポーネントは使用頻度少なめにする -->
 <script lang="ts">
+	import { css } from 'styled-system/css'
 	import type { PathName } from '$lib/route'
 	import { generatePath } from '$lib/route'
 	import { primaryButton } from '$ui/_button/PrimaryButton/recipe'
+	import TooltipIcon from '$ui/TooltipIcon/TooltipIcon.svelte'
 
 	export let title: string
 	export let pathKey: PathName
 	export let params: string | string[] | undefined = undefined
 	export let size: 'sm' | 'lg' = 'sm'
-	export let disabled: boolean = false
+	export let disabledReason: string = ''
+
+	$: disabled = !!disabledReason
 
 	$: formatParams = typeof params === 'string' ? [params] : params
 
 	$: href = generatePath(pathKey, formatParams)
+
+	const wrapper = css({
+		display: 'flex',
+		alignItems: 'center',
+		gap: '4px',
+		height: 'auto'
+	})
 </script>
 
-<a class={primaryButton({ size, disabled })} {href}>{title}</a>
+<div class={wrapper}>
+	<a class={primaryButton({ size, disabled })} {href}>{title}</a>
+	{#if disabled}
+		<TooltipIcon message={disabledReason} />
+	{/if}
+</div>
