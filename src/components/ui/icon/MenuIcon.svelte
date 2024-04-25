853b935b9606cb6e6ef8eb85396fc49914b1daca
yutaro_matsumoto <spurs03170629@gmail.com>
2024-04-25 23:06:45 +0900

    MenuIcon作成

---
+++ src/components/ui/icon/MenuIcon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let size: number = 16
+
+	const wrapper = css({
+		display: 'flex'
+	})
+</script>
+
+<div class={wrapper}>
+	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 14 14">
+		<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M1 3a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1m3.5-.5h9M1 7.5a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1M4.5 7h9M1 12a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1m3.5-.5h9"
+		/>
+	</svg>
+</div>
