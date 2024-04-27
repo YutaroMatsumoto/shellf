a7b52064531709c2a9e5cf99526e897064324c5e
yutaro_matsumoto <spurs03170629@gmail.com>
2024-04-27 11:18:25 +0900

    Deleteアイコン実装

---
+++ src/components/ui/icon/DeleteIcon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let size: number = 16
+
+	const wrapper = css({
+		display: 'flex'
+	})
+</script>
+
+<div class={wrapper}>
+	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 14 14">
+		<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M2.5 7V1.5a1 1 0 0 1 1-1H10L13.5 4v8.5a1 1 0 0 1-1 1H8M4.74 9.26L.5 13.5m0-4.24l4.24 4.24"
+		/>
+	</svg>
+</div>
