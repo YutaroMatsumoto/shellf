134e43dc5ba5bfe34660a36177044d2ab07d5438
yutaro_matsumoto <spurs03170629@gmail.com>
2024-05-03 15:53:58 +0900

    H3コンポーネント実装

---
+++ src/components/ui/_heading/H3/H3.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let title: string
+	/** fontsizeもpropsで受け取れた方が良いかも？ */
+
+	const h3 = css({
+		fontSize: 'l',
+		fontWeight: 'bold',
+		whiteSpace: 'pre-wrap'
+	})
+</script>
+
+<h3 class={h3}>{title}</h3>
