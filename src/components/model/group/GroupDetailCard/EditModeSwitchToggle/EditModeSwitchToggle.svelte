a2e9c18e21b7c93f2716b526ba6898d2fdd891ac
yutaro_matsumoto <spurs03170629@gmail.com>
2024-05-03 15:54:51 +0900

    編集モード切り替えようトグルの実装

---
+++ src/components/model/group/GroupDetailCard/EditModeSwitchToggle/EditModeSwitchToggle.svelte
@@ -0,0 +1,36 @@
+<!-- MEMO: formではない。画面における状態を切り替えるためのトグル。 -->
+<script lang="ts">
+	import {
+		circle,
+		fieldWrapper,
+		input,
+		toggleArea,
+		toggleWrapper
+	} from './editModeSwitchToggle.style'
+
+	export let label: string
+	export let isEditMode: boolean = false
+	export let onClick: () => void
+</script>
+
+<div class={fieldWrapper}>
+	<!-- MEMO: labelの領域をクリックして値が変わって欲しくないのでspanタグを利用 -->
+	<span>{label}</span>
+	<div class={toggleWrapper}>
+		<div class={toggleArea({ checked: isEditMode })}>
+			<div class={circle({ checked: isEditMode })} />
+		</div>
+		<button class={input} on:click={onClick} />
+	</div>
+</div>
+
+<style>
+	div {
+		display: flex;
+	}
+
+	span {
+		font-size: 16px;
+		font-weight: bold;
+	}
+</style>
---
+++ src/components/model/group/GroupDetailCard/EditModeSwitchToggle/editModeSwitchToggle.style.ts
@@ -0,0 +1,62 @@
+import type { SystemStyleObject } from 'styled-system/types'
+import { css, cva } from 'styled-system/css'
+
+export const fieldWrapper = css({
+	display: 'flex',
+	gap: '8px'
+})
+
+export const toggleWrapper = css({
+	position: 'relative',
+	width: '48px'
+})
+
+export const input = css({
+	position: 'absolute',
+	width: '100%',
+	height: '100%',
+	opacity: 0
+})
+
+export const circleDeafultStyle: SystemStyleObject = {
+	width: '20px',
+	height: '20px',
+	backgroundColor: 'white',
+	borderRadius: '50%',
+	transition: 'ease-in-out',
+	transitionDuration: '0.2s'
+}
+
+export const circle = cva({
+	base: circleDeafultStyle,
+	variants: {
+		checked: {
+			false: { transform: 'translateX(0px)' },
+			true: { transform: 'translateX(24px)' }
+		}
+	},
+	defaultVariants: {
+		checked: false
+	}
+})
+
+const toggleAreaDefaultStyle: SystemStyleObject = {
+	display: 'flex',
+	height: '24px',
+	width: '100%',
+	borderRadius: '44px',
+	padding: '2px'
+}
+
+export const toggleArea = cva({
+	base: toggleAreaDefaultStyle,
+	variants: {
+		checked: {
+			false: { backgroundColor: 'gray.300' },
+			true: { backgroundColor: 'green.300' }
+		}
+	},
+	defaultVariants: {
+		checked: false
+	}
+})
