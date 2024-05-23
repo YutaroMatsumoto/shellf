27913a48c3f79c3358c57b4086fee94b84ac3499
yutaro_matsumoto <spurs03170629@gmail.com>
2024-05-23 22:54:40 +0900

    EventHostGroupコンポーネントでSectionBoxWithTitleを利用

--- src/components/model/event/EventHostGroup/EventHostGroup.svelte
+++ src/components/model/event/EventHostGroup/EventHostGroup.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
+	import { css } from 'styled-system/css'
 	import type { GroupWithCreateUser } from '$models/group'
 	import SectionBoxWithTitle from '$ui/SectionBoxWithTitle/SectionBoxWithTitle.svelte'
-	import { hostGroupArea, groupImage } from './eventHostGroup.style'
 
 	export let name: GroupWithCreateUser['name']
 	export let src: GroupWithCreateUser['img_url']
+
+	const groupImage = css({
+		width: '100px',
+		height: '100px',
+		objectFit: 'contain',
+		border: '1px solid',
+		borderRadius: 'sm',
+		borderColor: 'gray.300'
+	})
+
+	const hostGroupArea = css({
+		width: '100%',
+		display: 'flex',
+		alignItems: 'center',
+		gap: '20px',
+		backgroundColor: 'white'
+	})
 </script>
 
 <SectionBoxWithTitle title="開催グループ">
