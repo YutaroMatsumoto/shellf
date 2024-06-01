320be774c2c159a76ee5d173f62a06baf9aba9cf
yutaro_matsumoto <spurs03170629@gmail.com>
2024-06-01 11:50:20 +0900

    MyPageTabの微修正

--- src/components/model/user/MyPageTab/MyPageTab.svelte
+++ src/components/model/user/MyPageTab/MyPageTab.svelte
@@ -1,6 +1,12 @@
-<script>
+<script lang="ts">
 	import Tab from '$ui/Tab/Tab.svelte'
-	import { tabs } from './tab'
+	import type { TabItemType } from '$ui/Tab/tab'
+
+	const tabs: TabItemType[] = [
+		{ href: '/my', title: '所属グループ', ariaControles: 'my-group' },
+		{ href: '/my/event', title: '作成したイベント', ariaControles: 'my-event' }
+		// { href: '/my/study', title: '参加予定', ariaControles: 'my-study' }
+	]
 </script>
 
 <Tab {tabs} />
--- src/components/model/user/MyPageTab/tab.ts
+++
@@ -1,7 +0,0 @@
-import type { TabItemType } from '$ui/Tab/tab'
-
-export const tabs: TabItemType[] = [
-	{ href: '/my', title: '所属グループ', ariaControles: 'my-group' },
-	{ href: '/my/event', title: '作成したイベント', ariaControles: 'my-event' }
-	// { href: '/my/study', title: '参加予定', ariaControles: 'my-study' }
-]
