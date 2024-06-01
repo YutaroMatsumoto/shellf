4e6ffdb311f7e9b9795b8e82459057a8ebfd4f53
yutaro_matsumoto <spurs03170629@gmail.com>
2024-06-01 20:00:36 +0900

    Header修正

--- src/components/ui/_layout/Header/Header.svelte
+++ src/components/ui/_layout/Header/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { css } from 'styled-system/css'
+	import ContentWidthController from '$ui/_layout/_variedLayout/ContentWidthController/ContentWidthController.svelte'
 	import Logo from '$ui/_layout/Logo/Logo.svelte'
 	import PrimaryButton from '$ui/_button/PrimaryButton/PrimaryButton.svelte'
 	import { getUser } from '$globalStates/user'
@@ -10,15 +11,19 @@
 	const header = css({
 		position: 'fixed',
 		top: '0',
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'space-between',
 		height: '60px',
 		width: '100%',
-		padding: '8px 24px',
 		bg: 'white',
 		shadow: 'sm',
 		zIndex: '100'
+	})
+
+	const headerContent = css({
+		height: '100%',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: '8px 24px'
 	})
 
 	const session = getSession()
@@ -27,10 +32,14 @@
 </script>
 
 <header class={header}>
-	<Logo />
-	{#if session}
-		<UserAvatarButton src={$user?.avatar_url} />
-	{:else if !session}
-		<PrimaryButton onClick={modal.login}>ログイン</PrimaryButton>
-	{/if}
+	<ContentWidthController havePadding={false}>
+		<div class={headerContent}>
+			<Logo />
+			{#if session}
+				<UserAvatarButton src={$user?.avatar_url} />
+			{:else if !session}
+				<PrimaryButton onClick={modal.login}>ログイン</PrimaryButton>
+			{/if}
+		</div>
+	</ContentWidthController>
 </header>
