cb7db204298424c36668bfad4da515663e7b2f5a
yutaro_matsumoto <spurs03170629@gmail.com>
2024-06-01 11:51:34 +0900

    色をwidthそれぞれにのみ関心を持つコンポーネントを実装

---
+++ src/components/ui/_layout/_variedLayout/ContentColorController/ContentColorController.svelte
@@ -0,0 +1,15 @@
+<!-- contentの背景色を設定するためのもの -->
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let backgroundColor: 'white' | 'gray.50' = 'white'
+
+	$: wrapper = css({
+		width: '100%',
+		backgroundColor
+	})
+</script>
+
+<div class={wrapper}>
+	<slot />
+</div>
---
+++ src/components/ui/_layout/_variedLayout/ContentWidthController/ContentWidthController.svelte
@@ -0,0 +1,17 @@
+<!-- contentの最大横幅を設定するためのもの -->
+<script lang="ts">
+	import { css } from 'styled-system/css'
+
+	export let havePadding: boolean = true
+
+	const wrapper = css({
+		maxWidth: '1280px',
+		minWidth: '768px', // ipad mini
+		padding: havePadding ? '24px' : '0px',
+		marginX: 'auto'
+	})
+</script>
+
+<div class={wrapper}>
+	<slot />
+</div>
