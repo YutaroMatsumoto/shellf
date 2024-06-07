d5ce5aefba47b95c9a872c3e91049e425aa2dfd3
yutaro_matsumoto <spurs03170629@gmail.com>
2024-06-07 13:04:41 +0900

    layout追加

--- src/components/ui/_layout/_variedLayout/SimpleExceptionalLayout/SimpleExceptionalLayout.svelte
+++ src/components/ui/_layout/_variedLayout/SimpleExceptionalLayout/SimpleExceptionalLayout.svelte
@@ -1,7 +1,21 @@
-<div><slot /></div>
+<!-- このコンポーネントの存在意義は改めて考える -->
+<!-- route, もしくはsrc/components/ui/_layoutのみで呼び出し可能とする -->
+<script>
+	import { css } from 'styled-system/css'
+	import TextLink from '$ui/TextLink/TextLink.svelte'
+	import ContentWidthController from '../ContentWidthController/ContentWidthController.svelte'
 
-<style>
-	div {
-		padding: 24px;
-	}
-</style>
+	const content = css({
+		display: 'flex',
+		gap: '16px',
+		flexDirection: 'column',
+		alignItems: 'flex-start'
+	})
+</script>
+
+<ContentWidthController>
+	<div class={content}>
+		<TextLink isBack text="前のページに戻る" />
+		<slot />
+	</div>
+</ContentWidthController>
