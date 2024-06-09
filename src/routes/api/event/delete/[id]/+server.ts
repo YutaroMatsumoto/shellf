2cc3ef373ee2371ed853f215222406329bd102f6
yutaro_matsumoto <spurs03170629@gmail.com>
2024-06-09 14:38:09 +0900

    グループ削除機能（サーバー側）

--- src/routes/api/event/delete/[id]/+server.ts
+++ src/routes/api/event/delete/[id]/+server.ts
@@ -1,11 +1,8 @@
 import type { RequestHandler } from './$types'
-
 import { json } from '@sveltejs/kit'
 
 // TODO: returnする値は見直す
 export const DELETE: RequestHandler = async ({ locals: { supabase }, params: { id } }) => {
 	const { data, error } = await supabase.from('event').delete().eq('id', id)
-
-	console.log({ id, data, error })
 	return json(error ?? data)
 }
